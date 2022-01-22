fd9ab996cced95e4d39dec5c6d63335b45774720
fatemehNmty <97907393+fatemehNmty@users.noreply.github.com>
2022-01-22 10:52:15 +0330

    function for day and time

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-let apiKey="29b613cb78020bf566b370de137d2f26";
-let city 
-let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
 
 function dispalyTemperature(response){
     let temperatureElement=document.querySelector("#degree");
@@ -12,4 +8,41 @@ function dispalyTemperature(response){
     let dayElement=document.querySelector("#day");
     let timeElement=document.querySelector("#time");
     console.log(response.data);
+    temperatureElement.innerHTML=Math.round(response.data.main.temp);
+    cityElement.innerHTML=response.data;
+    descriptionElement.innerHTML=response.data;
+    humidityElement.innerHTML=response.data;
+    windElement.innerHTML=response.data;
+    dayElement=day;
+    timeElement=(hours+":"+minutes);
+
 }
+function formatDay(timesstap){
+    let date=new Date(timesstap);
+    let day=date.getDay();
+    let hours=date.getHours();
+    let minutes=date.getMinutes();
+    let days=[ "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"];
+    let day=days[date.getDay()];
+    if(minutes<10){
+        minutes=("0"+minutes);
+    }
+    if(hours<10){
+        hours=("0"+hours);
+    }
+
+}
+
+
+
+
+let apiKey="29b613cb78020bf566b370de137d2f26";
+let city 
+let apiUrl=`https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+axios.get(apiUrl).then(dispalyTemperature);
